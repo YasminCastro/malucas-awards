0700c91ab854ec131c257dc30893459b9a449535
Yasmin <yasminsdcastro@gmail.com>
2024-10-06 12:30:30 -0300

    feat: category

--- components/Vote/index.tsx
+++ components/Vote/index.tsx
@@ -29,7 +29,7 @@ export default function Vote({ id, title, nominees }: IProps) {
       >
         {title}
       </AccordionTrigger>
-      <AccordionContent className="flex justify-center items-center min-h-screen  p-20">
+      <AccordionContent className="flex justify-center items-center min-h-screen">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {nominees.map((nominee) => (
             <NomineeCard
