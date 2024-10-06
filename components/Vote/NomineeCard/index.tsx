0a2eacbce32e11303d334344402dc84631b9af84
Yasmin <yasminsdcastro@gmail.com>
2024-10-06 12:27:34 -0300

    feat: is winner

--- components/Vote/NomineeCard/index.tsx
+++ components/Vote/NomineeCard/index.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 interface IProps {
   name: string;
   picture: string;
+  isWinner?: boolean;
 }
-export default function NomineeCard({ picture, name }: IProps) {
+export default function NomineeCard({ picture, name, isWinner }: IProps) {
   return (
     <div className="relative max-w-[380px] max-h-[380px] w-full h-full overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
       <div className="relative w-full h-full">
@@ -16,8 +17,13 @@ export default function NomineeCard({ picture, name }: IProps) {
           objectFit="cover"
         />
       </div>
-      <div className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent text-white">
-        <h3 className="text-xl font-bold">{name}</h3>
+      <div className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent flex flex-col gap-2 ">
+        {isWinner && (
+          <span className="bg-[#f93fff] text-black font-bold p-1 w-fit">
+            GANHADOR
+          </span>
+        )}
+        <h3 className="text-2xl font-bold  text-white">{name}</h3>
       </div>
     </div>
   );
