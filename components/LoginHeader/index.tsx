ee9185015b68e631670b7436f95cd3023376edd1
Yasmin <yasminsdcastro@gmail.com>
2024-10-18 15:31:05 -0300

    fix: header size for phone

--- components/LoginHeader/index.tsx
+++ components/LoginHeader/index.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-
 export default function LoginHeader() {
   return (
-    <div className="bg-[#f7f908] flex justify-center items-center flex-col p-14 gap-3">
-      <h1 className="font-bold text-5xl">Votação Aberta!</h1>
-      <h2 className="font-bold text-xl">Faça login para poder votar</h2>
+    <div className="bg-[#f7f908] flex justify-center items-center flex-col p-14 gap-3 max-md:p-8">
+      <h1 className="font-bold text-5xl max-md:text-3xl">Votação Aberta!</h1>
+      <h2 className="font-bold text-xl max-md:text-base">
+        Faça login para poder votar
+      </h2>
     </div>
   );
 }
