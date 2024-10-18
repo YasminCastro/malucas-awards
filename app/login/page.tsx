e9793b9a929f8d39c82d0ac87e6bf114bb61f2cf
Yasmin <yasminsdcastro@gmail.com>
2024-10-18 16:51:21 -0300

    feat: login and signup

---
+++ app/login/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(2, {
+    message: "Password must be at least 2 characters.",
+  }),
+});
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-1/3 border mx-auto backdrop-blur-xl bg-white/30 p-8 rounded-lg">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={80}
+          height={80}
+          className="mb-4"
+        />
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-3 mb-3"
+          >
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite seu @ do instagram" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite sua senha" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Entrar
+            </Button>
+          </form>
+          <Link href="/signup">Clique aqui para fazer o seu cadastro!</Link>
+        </Form>
+      </div>
+    </div>
+  );
+}
--- components/LoginHeader/index.tsx
+++ components/LoginHeader/index.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 export default function LoginHeader() {
   return (
-    <div className="bg-[#f7f908] flex justify-center items-center flex-col p-14 gap-3 max-md:p-8">
+    <Link
+      className="bg-[#f7f908] flex justify-center items-center flex-col p-14 gap-3 max-md:p-8"
+      href="/login"
+    >
       <h1 className="font-bold text-5xl max-md:text-3xl">Votação Aberta!</h1>
       <h2 className="font-bold text-xl max-md:text-base">
         Faça login para poder votar
       </h2>
-    </div>
+    </Link>
   );
 }
