d8d899ca961538ba2a6d351a201ddb92a34b224b
Yasmin <yasminsdcastro@gmail.com>
2025-01-21 13:51:19 -0300

    dia da votação

--- components/LoginHeader/index.tsx
+++ components/LoginHeader/index.tsx
@@ -2,6 +2,14 @@ import Link from "next/link";
 
 export default function LoginHeader({ isLoggedIn }: { isLoggedIn: boolean }) {
   return (
+    <div className="bg-[#f7f908] flex justify-center items-center flex-col p-14 gap-3 max-md:p-8">
+      <h1 className="font-bold text-5xl max-md:text-3xl">
+        Votação irá abrir dia 27/01!
+      </h1>
+    </div>
+  );
+
+  return (
     <Link
       className="bg-[#f7f908] flex justify-center items-center flex-col p-14 gap-3 max-md:p-8"
       href="/login"
