2f8fe908003b0a3cbb286bf3da5e64566eea6606
Yasmin <yasminsdcastro@gmail.com>
2025-02-05 08:39:23 -0300

    feat: login

--- components/LoginHeader/index.tsx
+++ components/LoginHeader/index.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
 
 export default function LoginHeader({ isLoggedIn }: { isLoggedIn: boolean }) {
-  return (
-    <div className="bg-[#f7f908] flex justify-center items-center flex-col p-14 gap-3 max-md:p-8">
-      <h1 className="font-bold text-5xl max-md:text-3xl">
-        Votação irá abrir dia 05/02!
-      </h1>
-    </div>
-  );
+  // return (
+  //   <div className="bg-[#f7f908] flex justify-center items-center flex-col p-14 gap-3 max-md:p-8">
+  //     <h1 className="font-bold text-5xl max-md:text-3xl">
+  //       Votação irá abrir dia 05/02!
+  //     </h1>
+  //   </div>
+  // );
+
+  console.log(isLoggedIn);
+
+  if (isLoggedIn) {
+    return (
+      <div className="bg-[#f7f908] flex justify-center items-center flex-col p-14 gap-3 max-md:p-8">
+        <h1 className="font-bold text-5xl max-md:text-3xl">Votação Aberta!</h1>
+      </div>
+    );
+  }
 
   return (
     <Link
@@ -17,7 +27,7 @@ export default function LoginHeader({ isLoggedIn }: { isLoggedIn: boolean }) {
       <h1 className="font-bold text-5xl max-md:text-3xl">Votação Aberta!</h1>
       {!isLoggedIn && (
         <h2 className="font-bold text-xl max-md:text-base">
-          Faça login para poder votar
+          Clique aqui para fazer login e votar
         </h2>
       )}
     </Link>
